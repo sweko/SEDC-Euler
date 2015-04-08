4ff778d14b2c66312e9ddcbbba65e1e36d7883d2
Darko Kotoski <wbountyhunter@hotmail.com>
2015-04-08 04:01:58 +0200

    Site update
    Added a page for time scores of every solver

--- EulerDisplay/Controllers/HomeController.cs
+++ EulerDisplay/Controllers/HomeController.cs
@@ -43,12 +43,62 @@ namespace EulerDisplay.Controllers
             executor.RegisterSolver(new BaseSolver { Name = "Nikola Andreev" });
             executor.RegisterSolver(new BaseSolver { Name = "Darko Hristovski" });
             executor.RegisterSolver(new BaseSolver { Name = "Daniel Bibovski" });
-            
+
             var runResults = executor.RunSolvers().OrderByDescending(rr => rr.TotalSolved).ThenBy(rr => rr.Name).ToList();
 
             ViewBag.Title = "Student Leaderboard";
 
             return View(runResults);
         }
+
+        public ActionResult Personal(string Solver)
+        {
+            Executor executor = new Executor();
+            var solvers = Assembly
+                .GetAssembly(typeof(SWekoSolver))
+                .GetTypes()
+                .Where(t => t.GetInterfaces().Contains(typeof(ISolver)))
+                .Select(Activator.CreateInstance)
+                .Cast<ISolver>();
+
+            foreach (var solver in solvers)
+            {
+                executor.RegisterSolver(solver);
+            }
+
+            executor.RegisterSolver(new BaseSolver { Name = "Darko Ivanov" });
+            executor.RegisterSolver(new BaseSolver { Name = "Darko Kostadinov" });
+            executor.RegisterSolver(new BaseSolver { Name = "Goran Tozievski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Gorast Cvetkovski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Vladimir Totochevski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Jovan Cokleski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Petar Papalevski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Ilche Ivanovski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Kristijan Arsovski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Monika Jovanova" });
+            executor.RegisterSolver(new BaseSolver { Name = "Ervin Jonuzoski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Mitko Miloshev" });
+            executor.RegisterSolver(new BaseSolver { Name = "Martin Josifovski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Ice Jovanoski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Nikola Andreev" });
+            executor.RegisterSolver(new BaseSolver { Name = "Darko Hristovski" });
+            executor.RegisterSolver(new BaseSolver { Name = "Daniel Bibovski" });
+
+            var runResults = executor.RunSolvers().OrderByDescending(rr => rr.TotalSolved).ThenBy(rr => rr.Name).ToList();
+
+            ViewBag.Title = "Student Leaderboard";
+
+            ViewBag.Title = "Student Leaderboard";
+
+            var result = runResults.FirstOrDefault(s => s.Name == Solver);
+
+            if (result == null)
+            {
+                Redirect("~/Error");
+            }
+
+            return View(result);
+        }
+
     }
 }
